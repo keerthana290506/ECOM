45ffcbfb7d532fa5884f421af4dd69b26996818c
keerthana290506 <keerthana290506.com>
2025-03-10 15:24:31 +0530

    mil1

---
+++ fronts/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
